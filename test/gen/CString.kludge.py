6aaae3baa0caef6fa21f5e217bb3364045f65305
Peter Zion <peter.zion@gmail.com>
2016-08-26 11:06:46 -0400

    gen: convert CString test

---
+++ test/gen/CString.kludge.py
@@ -0,0 +1,27 @@
+ext.add_cpp_quoted_include('CString.hpp')
+
+ext.add_func('CStringParams')\
+  .returns('const char *')\
+  .add_param('char const *')\
+  .add_param('char const * const &')\
+  .add_test("""
+report("CStringParams('value', 'constRef') = " + CStringParams('value', 'constRef'));
+""", """
+CStringParams('value', 'constRef') = valueconstRef
+""")
+
+ext.add_func('CStringValueReturn')\
+  .returns('char const *')\
+  .add_test("""
+report("CStringValueReturn() = " + CStringValueReturn());
+""", """
+CStringValueReturn() = value
+""")
+
+ext.add_func('CStringConstRefReturn')\
+  .returns('char const * const &')\
+  .add_test("""
+report("CStringConstRefReturn() = " + CStringConstRefReturn());
+""", """
+CStringConstRefReturn() = constRef
+""")

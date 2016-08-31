aa308a514200972e813e909589de27069fa01d70
Peter Zion <peter.zion@gmail.com>
2016-08-31 16:47:29 -0400

    More fixes for USD

--- libkludge/types/direct/__init__.py
+++ libkludge/types/direct/__init__.py
@@ -75,30 +75,9 @@ class DirectSelector(Selector):
         )
       return result
 
-    is_reference = isinstance(cpp_type_expr, ReferenceTo)
-    is_pointer = isinstance(cpp_type_expr, PointerTo)
-
-    # FIXME special case, e.g. "Class * &" we drop to just "Class *"
-    if is_reference and isinstance(cpp_type_expr.pointee, PointerTo):
-      cpp_type_expr = cpp_type_expr.pointee
-      is_pointer = True
-      is_reference = False
-
-    if (is_pointer or is_reference) \
-      and (isinstance(cpp_type_expr.pointee, Named) or \
-        isinstance(cpp_type_expr.pointee, Template)):
-      undq_cpp_type_expr, _ = cpp_type_expr.pointee.get_undq_type_expr_and_dq()
+    if isinstance(cpp_type_expr, Indirect):
+      undq_cpp_type_expr, dq = cpp_type_expr.get_undq_type_expr_and_dq()
       if undq_cpp_type_expr == self.cpp_type_expr:
-        if is_pointer:
-          if cpp_type_expr.pointee.is_const:
-            dq = dir_qual.const_pointer
-          else:
-            dq = dir_qual.mutable_pointer
-        else:
-          if cpp_type_expr.pointee.is_const:
-            dq = dir_qual.const_reference
-          else:
-            dq = dir_qual.mutable_reference
         return DirQualTypeInfo(
           dq,
           DirectTypeInfo(

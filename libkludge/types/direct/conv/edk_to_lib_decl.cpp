2d11399b09b221e8f73d41841969af2467abc6ee
Peter Zion <peter.zion@gmail.com>
2016-09-01 19:20:01 -0400

    gen: fixes for direct type

--- libkludge/types/direct/conv/edk_to_lib_decl.cpp
+++ libkludge/types/direct/conv/edk_to_lib_decl.cpp
@@ -1,4 +1,5 @@
 {######################################################################}
 {# Copyright (c) 2010-2016, Fabric Software Inc. All rights reserved. #}
 {######################################################################}
-{{conv.type_info.lib.name.base}} &{{conv.value_name.lib}} = *{{conv.value_name.edk}}.cpp_ptr;
+{{conv.type_info.lib.name.base}} &{{conv.value_name.lib}} =
+  *static_cast< ::{{conv.type_info.lib.name.base}} * >( {{conv.value_name.edk}}.cpp_ptr );
--- libkludge/types/direct/conv/lib_to_edk.cpp
+++ libkludge/types/direct/conv/lib_to_edk.cpp
@@ -3,6 +3,6 @@
 {######################################################################}
 if ( &{{conv.value_name.lib}} != {{conv.value_name.edk}}.cpp_ptr )
 {
-  delete {{conv.value_name.edk}}.cpp_ptr;
+  delete static_cast< ::{{conv.type_info.lib.name.base}} * >( {{conv.value_name.edk}}.cpp_ptr );
   {{conv.value_name.edk}}.cpp_ptr = new {{conv.type_info.lib.name.compound}}( {{conv.value_name.lib}} );
 }

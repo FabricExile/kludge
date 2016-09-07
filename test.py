34796c3758a46a89d3d044d3bbbb6a89e6c2b5fc
Peter Zion <peter.zion@gmail.com>
2016-09-06 21:41:39 -0400

    Unit test setup

--- test.py
+++ test.py
@@ -6,9 +6,9 @@
 import os, pytest, subprocess, difflib
 
 test_generate_dir = os.path.join('test', 'generate')
-test_tmp_dir_base = os.path.join('test', 'tmp')
+test_generate_tmp_dir_base = os.path.join('test', 'tmp', 'generate')
 
-def collect_test_gen_kludge_py_basenames():
+def collect_test_generate_basenames():
   result = []
   for dirpath, _subdirs, filenames in os.walk(test_generate_dir):
       for filename in filenames:
@@ -19,13 +19,13 @@ def collect_test_gen_kludge_py_basenames():
                 result.append(filebase)
   return result
 
-@pytest.mark.parametrize("kludge_py_basename", collect_test_gen_kludge_py_basenames())
-def test_kludge_py_basename(kludge_py_basename):
+@pytest.mark.parametrize("kludge_py_basename", collect_test_generate_basenames())
+def test_generate(kludge_py_basename):
 
   if os.path.isfile(os.path.join(test_generate_dir, kludge_py_basename + '.skip')):
     return
 
-  test_tmp_dir = os.path.join(test_tmp_dir_base, kludge_py_basename)
+  test_tmp_dir = os.path.join(test_generate_tmp_dir_base, kludge_py_basename)
 
   if not os.path.isdir(test_tmp_dir):
     os.makedirs(test_tmp_dir)
@@ -67,3 +67,63 @@ def test_kludge_py_basename(kludge_py_basename):
     print diffline
     diffline_count += 1
   assert diffline_count == 0
+
+test_discover_dir = os.path.join('test', 'discover')
+test_discover_tmp_dir_base = os.path.join('test', 'tmp', 'discover')
+
+def collect_test_discover_basenames():
+  result = []
+  for dirpath, _subdirs, filenames in os.walk(test_discover_dir):
+      for filename in filenames:
+          basename, fileext = os.path.splitext(filename)
+          if fileext == '.hpp':
+            result.append(basename)
+  return result
+
+@pytest.mark.parametrize("basename", collect_test_discover_basenames())
+def test_discover(basename):
+
+  if os.path.isfile(os.path.join(test_discover_dir, basename + '.skip')):
+    return
+
+  test_tmp_dir = os.path.join(test_discover_tmp_dir_base, basename)
+
+  if not os.path.isdir(test_tmp_dir):
+    os.makedirs(test_tmp_dir)
+
+  assert subprocess.call(
+    [
+      './kludge', 'discover',
+      '-o', test_tmp_dir,
+      os.path.join(test_discover_dir, basename + '.hpp'),
+      ],
+    ) == 0
+
+  # scons_env = os.environ.copy()
+  # scons_env['CPPPATH'] = os.path.abspath(test_discover_dir)
+  # assert subprocess.call(
+  #   [
+  #     'scons',
+  #     '-f', kludge_py_basename + '.SConstruct',
+  #     ],
+  #   cwd = test_tmp_dir,
+  #   env = scons_env,
+  #   ) == 0
+
+  # kl_env = os.environ.copy()
+  # kl_env['FABRIC_EXTS_PATH'] = test_tmp_dir
+  # with open(os.path.join(test_tmp_dir, kludge_py_basename + '.test.out')) as expected_kl_output_file:
+  #   expected_kl_output = expected_kl_output_file.read().splitlines()
+  # actual_kl_output = subprocess.check_output(
+  #   [
+  #     'kl',
+  #     os.path.join(test_tmp_dir, kludge_py_basename + '.test.kl'),
+  #     ],
+  #   env = kl_env,
+  #   ).splitlines()
+  # difflines = difflib.unified_diff(expected_kl_output, actual_kl_output)
+  # diffline_count = 0
+  # for diffline in difflines:
+  #   print diffline
+  #   diffline_count += 1
+  # assert diffline_count == 0

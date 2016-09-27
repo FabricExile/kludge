88d01dc55b9103910e2180c4ff7579fb5970f057
Helge Mathee <helge.mathee@fabricengine.com>
2016-09-27 15:00:03 +0200

    add unit test for abstract class

---
+++ test/discover/AbstractClass.hpp
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2010-2016, Fabric Software Inc. All rights reserved.
+//
+
+#pragma once
+
+class MyAbstractClass
+{
+public:
+  virtual const char *GetData(int index) const = 0;
+  virtual const char *GetData() const
+  {
+    return "MyAbstractClass";
+  }
+};

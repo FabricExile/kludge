56d72a43ca615d3a4ea256b780ca67bbf3c3ab59
Peter Zion <peter.zion@gmail.com>
2017-02-15 13:10:27 -0500

    Minor cleanup

--- libkludge/types/fixed_array/conv/edk_to_lib_decl.cpp
+++ libkludge/types/fixed_array/conv/edk_to_lib_decl.cpp
@@ -1,7 +1,6 @@
 {############################################################################}
 {# Copyright (c) 2010-2017 Fabric Software Inc. All rights reserved.        #}
 {############################################################################}
-
 {% set r = conv.type_info.lib.expr.range %}
 {% for i in r %}
 {{conv.child[0].type_info.edk.name}} const &{{conv.child[0].value_name.edk}}__{{i}} = {{conv.value_name.edk}}[{{i}}];
--- libkludge/types/fixed_array/conv/lib_to_edk.cpp
+++ libkludge/types/fixed_array/conv/lib_to_edk.cpp
@@ -1,7 +1,6 @@
 {############################################################################}
 {# Copyright (c) 2010-2017 Fabric Software Inc. All rights reserved.        #}
 {############################################################################}
-
 for ( uint32_t _KLUDGE_INDEX = 0; _KLUDGE_INDEX < {{conv.type_info.lib.expr.size}}; ++_KLUDGE_INDEX )
 {
     {{conv.child[0].type_info.lib.name.base}} const &{{conv.child[0].value_name.lib}}{{conv.child[0].type_info.lib.name.suffix}} = {{conv.child[0].deref_pointer_prefix}}{{conv.value_name.lib}}[_KLUDGE_INDEX];

9cfcb27119164d9615d2cafcd587bb85fb9caa1e
Helge Mathee <helge.mathee@fabricengine.com>
2017-04-30 19:33:47 +0200

    cxx_delete to also set the cpp_ptr to NULL

--- libkludge/types/managed/repr/delete.cpp
+++ libkludge/types/managed/repr/delete.cpp
@@ -2,4 +2,7 @@
 {# Copyright (c) 2010-2017 Fabric Software Inc. All rights reserved.        #}
 {############################################################################}
 if({{this.value_name.edk}}.cpp_ptr != NULL)
+{
   delete({{this.value_name.edk}}.cpp_ptr);
+  {{this.value_name.edk}}.cpp_ptr = NULL;
+}
